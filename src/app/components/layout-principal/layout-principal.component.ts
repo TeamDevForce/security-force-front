067e000e6c3d0681204e7cfe4743e7ceeb90d3e4
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2024-10-23 17:23:12 -0300

    SEC-92 - [feat]: Implementação: Adicionar e mover categorias

--- src/app/components/layout-principal/layout-principal.component.ts
+++ src/app/components/layout-principal/layout-principal.component.ts
@@ -1,17 +1,23 @@
+import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-layout-principal',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, DragDropModule],
   templateUrl: './layout-principal.component.html',
   styleUrl: './layout-principal.component.css'
 })
 export class LayoutPrincipalComponent {
+  @ViewChild('categoriesList') categoriesList!: ElementRef;
 
   colors: string[] = ['#9C5ECD', '#416FE3', '#5ECD70', '#E3A341', '#E34141', '#ffffff'];
+
+  categories = [
+    { title: 'Redes', color: '#ff0000' },
+  ];
 
   inputCategory: string = '';
   selectedColor: string = '#ffffff';
@@ -25,19 +31,52 @@ export class LayoutPrincipalComponent {
     this.isActive = true;
   }
 
+
   newCategory(event: KeyboardEvent) {
+    let shouldReset = false;
+
     if (event.key === 'Enter') {
       const newCategoryObject = {
         title: this.inputCategory,
         color: this.selectedColor
       }
+      this.categories.push(newCategoryObject);
+      this.scrollToLastCategory();
+      shouldReset = true;
+    } else if (event.key === 'Escape') {
+      this.isActive = false;
+      shouldReset = true;
+    }
 
+    if (shouldReset) {
       this.isActive = false;
       this.inputCategory = '';
       this.selectedColor = '#ffffff';
-    } else if (event.key === 'Escape') {
-      this.isActive = false;
+    }
+
+  }
+
+  hasScrolled = false;
+  isAtBottom = false;
+
+  scrollToLastCategory() {
+    if (this.categoriesList && this.categoriesList.nativeElement) {
+      const lastCategoryElement = this.categoriesList.nativeElement.lastElementChild;
+
+      if (lastCategoryElement) {
+        const element = this.categoriesList.nativeElement;
+        lastCategoryElement.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+
+        setTimeout(() => {
+          element.scrollTop = element.scrollHeight;
+        }, 300);
+      }
     }
   }
+
+  drop(event: CdkDragDrop<any[]>) {
+    moveItemInArray(this.categories, event.previousIndex, event.currentIndex);
+  }
+
 
 }
